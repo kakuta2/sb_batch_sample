56f81a1b552d9941e7df3d3cf274e231fbf4f1bc
kaku2 <kaku2@test.com>
2021-03-15 06:46:53 +0900

    for ST001

--- src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
+++ src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
@@ -1,12 +1,28 @@
 package com.example.batchprocessing;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.boot.CommandLineRunner;
 
+/**
+ * メイン　
+ * ST001対応
+ */
 @SpringBootApplication
-public class BatchProcessingApplication {
+public class BatchProcessingApplication  implements CommandLineRunner {
 
 	public static void main(String[] args) throws Exception {
 		System.exit(SpringApplication.exit(SpringApplication.run(BatchProcessingApplication.class, args)));
 	}
+
+	@Autowired
+	JdbcTemplate j;
+  
+	@Override
+	public void run(String... args) throws Exception {
+	  System.out.println("***********" + j.queryForObject("select count(*) from people", Integer.class));
+	}
+  
 }
